355ea1e5744835c219bc5445fb46935d672ea055
Wayne Walker <wwalker@solid-constructs.com>
2011-03-20 09:37:05 -0500

    added left 0 padding for values < 0x10

--- strtohex.rb
+++ strtohex.rb
@@ -2,9 +2,24 @@
 def string_to_hex(str)
   returned = ''
   for i in (0..str.length)
-    unless str[i].nil?
-      returned += str[i].to_s(16)
+    unless str[i].nil? # I don't think this can ever be nil
+      value = str[i].to_s(16)
+      if value.length == 1
+        value = "0" + value
+      end
+      returned += value
     end
   end
   returned
+end
+
+def s2h(str)
+  output = ''
+  str.bytes{ |x|
+    h = (x & 240) >> 4
+    l = x & 15
+    output += h.to_s(16)
+    output += l.to_s(16)
+  }
+  output
 end
